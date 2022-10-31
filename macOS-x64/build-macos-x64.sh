e96257129eb57e4ace42f08c5183da9976bdd21e
Dawid Owoc <dawidowoc6@gmail.com>
2022-10-31 18:00:59 +0100

    Don't ask for signing an installer

--- macOS-x64/build-macos-x64.sh
+++ macOS-x64/build-macos-x64.sh
@@ -164,13 +164,6 @@ function createInstaller() {
     log_info "Application installer generation process started.(3 Steps)"
     buildPackage
     buildProduct ${PRODUCT}-macos-installer-x64-${VERSION}.pkg
-    while true; do
-        read -p "Do you wish to sign the installer (You should have Apple Developer Certificate) [y/N]?" answer
-        [[ $answer == "y" || $answer == "Y" ]] && FLAG=true && break
-        [[ $answer == "n" || $answer == "N" || $answer == "" ]] && log_info "Skipped signing process." && FLAG=false && break
-        echo "Please answer with 'y' or 'n'"
-    done
-    [[ $FLAG == "true" ]] && signProduct ${PRODUCT}-macos-installer-x64-${VERSION}.pkg
     log_info "Application installer generation steps finished."
 }
 
